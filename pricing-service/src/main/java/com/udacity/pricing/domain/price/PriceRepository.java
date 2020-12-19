328d128045ebde4812adff50732ed23fdeae1ef2
Amador Cuenca <sphi02ac@gmail.com>
2020-12-19 09:49:40 -0300

    feat: Add missing controller

--- pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
@@ -1,12 +1,12 @@
 package com.udacity.pricing.domain.price;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PriceRepository extends CrudRepository<Price, Long> {
+public interface PriceRepository extends JpaRepository<Price, Long> {
 
     @RestResource(path = "byVehicleId")
     Price findByVehicleId(@Param("id") Long vehicleId);
